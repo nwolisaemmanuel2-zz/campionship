89e4f84d36e2e3dcd0277c69f37fa3d4c0b25400
Nwolisa Emmanuel Ekene <47080616+nwolisaemmanuel2@users.noreply.github.com>
2020-01-23 00:05:38 +0000

    Add files via upload

---
+++ db.js
@@ -0,0 +1,10 @@
+"use strict";
+//var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost/championgg');
+//var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
+	//console.log('Connection Made!');
+});
