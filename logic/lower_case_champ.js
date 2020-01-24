d475fa0ab06eaafea03fa156d2fa12f5358b3cc8
Nwolisa Emmanuel Ekene <47080616+nwolisaemmanuel2@users.noreply.github.com>
2020-01-24 14:09:13 +0000

    Update lower_case_champ.js

--- logic/lower_case_champ.js
+++ logic/lower_case_champ.js
@@ -8,13 +8,13 @@ var champList = require('../api_data/champions.json');
  * @param  {string} champName - the champ name to compare against the champList.
  * @return {string|undefined} - if a match is found, the champ key is returned, otherwise undefined.
  */
-var lowerCaseChamp = function(champName) {
-    for (var prop in champList) {
-        if (prop.toLowerCase() === champName.toLowerCase()) {
-            return prop;
-        }
-    }
-};
 
+  var lowerCaseChamp = function(champName) {
+       for (var prop in champList) {
+           if (prop.toLowerCase() === champName.toLowerCase()) {
+               return prop;
+           }
+       }
+   };
 
 module.exports = lowerCaseChamp;
