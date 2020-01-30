20d627ae35bc0c90994bd00f8159902f9d07f51c
Nwolisa Emmanuel Ekene <47080616+nwolisaemmanuel2@users.noreply.github.com>
2020-01-30 09:17:11 +0000

    Update championgg_tooltip.js

--- public/js/championgg_tooltip.js
+++ public/js/championgg_tooltip.js
@@ -3,6 +3,7 @@
 	var champggTooltip = angular.module('championggTooltip', []);
 
 	
+	
 	champggTooltip.filter('to_trusted', ['$sce', function($sce) {
 		return function(text) {
 			return $sce.trustAsHtml(text);
